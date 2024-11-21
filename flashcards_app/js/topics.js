f9addd6169b642f66d446e97cd4bc04636593e7e
4314 Пуртов Н.С. <1nikita05@mail.ru>
2024-11-21 11:53:33 +0300

    Remove unused flashcards_app folder

--- flashcards_app/js/topics.js
+++
@@ -1,4 +0,0 @@
-function selectTopic(topic) {
-    localStorage.setItem('selectedTopic', topic); // Сохраняем выбранную тему
-    window.location.href = 'cards.html';          // Переходим на страницу карточек
-}
