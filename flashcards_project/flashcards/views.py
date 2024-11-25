f9860be92d0a831b7d51315e5d8c098f662c9438
4314 Пуртов Н.С. <1nikita05@mail.ru>
2024-11-25 12:52:12 +0300

    Fix card display for multiple topics and add shuffling

--- flashcards_project/flashcards/views.py
+++ flashcards_project/flashcards/views.py
@@ -3,20 +3,32 @@ from rest_framework.response import Response
 from .models import Card, Topic
 from .serializers import CardSerializer
 from django.shortcuts import render
+from urllib.parse import unquote
 import random
 
+from urllib.parse import unquote
+
 def training(request):
-    # Получаем выбранные темы из параметров запроса
-    selected_topics = request.GET.getlist('topics')
+    # Получаем список выбранных тем из параметров запроса
+    selected_topics = request.GET.get('topics', '')  # Получаем строку с темами
+    selected_topics = unquote(selected_topics)  # Декодируем URL
+
+    if selected_topics:  # Если строка с темами не пуста
+        # Разделяем строку на отдельные темы
+        selected_topics_list = [topic.strip() for topic in selected_topics.split(',')]
+    else:
+        selected_topics_list = []  # Если тем нет, возвращаем пустой список
 
     # Фильтруем карточки по выбранным темам
-    if selected_topics:
-        cards = Card.objects.filter(topic__name__in=selected_topics)
+    if selected_topics_list:
+        cards = Card.objects.filter(topic__name__in=selected_topics_list)
     else:
         cards = Card.objects.all()  # Если темы не выбраны, возвращаем все карточки
 
-    # Ограничиваем тренировку 10 карточками
-    cards = cards[:10]
+    # Преобразуем QuerySet в список, перемешиваем и ограничиваем тренировку 10 карточками
+    cards = list(cards)  # Преобразуем QuerySet в список
+    random.shuffle(cards)  # Перемешиваем карточки
+    cards = cards[:10]  # Ограничиваем тренировку 10 карточками
 
     # Передаем карточки в шаблон
     return render(request, 'flashcards/training.html', {'cards': cards})
@@ -45,4 +57,3 @@ def index(request):
 def cards(request):
     topic = request.GET.get('topic', None)
     return render(request, 'flashcards/cards.html', {'topic': topic})
-
